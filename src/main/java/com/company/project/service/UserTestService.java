2f631daa11507fb293462af00540f3d8b4969db6
Gaochunlei425 <383834050@qq.com>
2020-11-28 17:58:12 +0800

    测试模板

---
+++ src/main/java/com/company/project/service/UserTestService.java
@@ -0,0 +1,11 @@
+package com.company.project.service;
+import com.company.project.model.UserTest;
+import com.company.project.core.Service;
+
+
+/**
+ * Created by CodeGenerator on 2020/11/28.
+ */
+public interface UserTestService extends Service<UserTest> {
+
+}
